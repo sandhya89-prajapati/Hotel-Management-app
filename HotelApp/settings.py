749ff9013b4ad880de27ac99dc6dbb45eb31cc1f
sandhya prajapati <sandhya89.prajapati@gmail.com>
2022-07-17 10:26:36 +0530

    my forth commit

--- HotelApp/settings.py
+++ HotelApp/settings.py
@@ -23,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e7^#wab%t-39+s!y!lgcrlxfqw%ywtxo#zp8elif58n^8u#h9j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
